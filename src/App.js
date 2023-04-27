a8569590efaa58c8cfb135d62d1feb036019e406
Rubby <rubenslombard@pursuit.org>
2023-04-27 11:38:43 -0400

    add a new building file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Lombarudi24
         </a>
       </header>
     </div>
