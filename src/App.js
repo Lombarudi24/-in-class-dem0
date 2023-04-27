4197ae5a15891de3bbf0315821e6254b9272dad2
Rubby <rubenslombard@pursuit.org>
2023-04-27 12:22:19 -0400

    added key files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@
 import './App.css';
 
 function App() {
+
+console.log(process.env)
   return (
     <div className="App">
-     Simple react
+      Simple react
+      <div>Current Env: {process.env.NODE_ENV}</div>
+      <div>Current API {process.env.NODE_ENV === "development"
+        ? process.env REACT_APP_DEVELOPMENT_API_KEY 
+        : process.env.REACT_APP_DEVELOPMENT_API_KEY}</div>
     </div>
   );
 }
